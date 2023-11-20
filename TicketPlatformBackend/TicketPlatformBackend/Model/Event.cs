9e87ee6ba6da991adc48101195c5ffac87cce08d
andrei22george <andrei22george@gmail.com>
2023-11-20 16:06:28 +0200

    Change Event accsesibility

--- TicketPlatformBackend/TicketPlatformBackend/Model/Event.cs
+++ TicketPlatformBackend/TicketPlatformBackend/Model/Event.cs
@@ -3,7 +3,7 @@
 namespace TicketPlatformBackend.Model
 {
     [Table("Event")]
-    internal class Event : Entity<int>
+    public class Event : Entity<int>
     {
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
