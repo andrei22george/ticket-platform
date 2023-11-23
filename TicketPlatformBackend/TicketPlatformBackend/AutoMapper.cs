ff4d4a1651d9608f3d79938b2158ccd42fefe3ff
andrei22george <andrei22george@gmail.com>
2023-11-23 08:26:52 +0200

    Add auto mapper

---
+++ TicketPlatformBackend/TicketPlatformBackend/AutoMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TicketPlatformBackend.Model;  
+using TicketPlatformBackend.Model.In;
+
+namespace TicketPlatformBackend
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<AdminIn, Admin>();
+            CreateMap<UserIn, User>();
+            CreateMap<EventIn, Event>();
+            CreateMap<TicketIn, Ticket>();
+        }
+    }
+}
