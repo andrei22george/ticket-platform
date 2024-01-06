b532acd2b9bb5d951cf4d635ad8c6828b4871deb
andrei22george <andrei22george@gmail.com>
2024-01-06 17:35:20 +0200

    Add services for card, cart, favourites and login

---
+++ TicketPlatform.API/TicketPlatform.API/Services/CartService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using ErrorOr;
+using TicketPlatform.API.Model.In;
+using TicketPlatform.API.Model;
+using TicketPlatform.API.Repositories.Interfaces;
+
+namespace TicketPlatform.API.Services
+{
+    public class CartService
+    {
+        private ICartRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CartService(ICartRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public List<Cart> GetAllCarts(QueryParameters parameters)
+        {
+            return _repository.GetAllCarts(parameters);
+        }
+
+        public ErrorOr<Cart> GetCartById(int id)
+        {
+            return _repository.GetCartByUserId(id);
+        }
+
+        public int InsertCart(CartIn eventIn)
+        {
+            var cart = _mapper.Map<Cart>(eventIn);
+
+            return _repository.InsertCart(cart);
+        }
+
+        public bool UpsertCart(int id, CartIn eventIn)
+        {
+            var cart = _mapper.Map<Cart>(eventIn);
+
+            return _repository.UpsertCart(id, cart);
+        }
+
+        public bool DeleteCart(int id)
+        {
+            return _repository.DeleteCart(id);
+        }
+
+        public bool DeleteCarts(List<int> ids)
+        {
+            return _repository.DeleteCarts(ids);
+        }
+    }
+}
---
+++ TicketPlatform.API/TicketPlatform.API/Services/LoginService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using TicketPlatform.API.Model;
+using TicketPlatform.API.Repositories.Interfaces;
+
+namespace TicketPlatform.API.Services
+{
+    public class LoginService
+    {
+        private ILoginRepository _repository;
+        private readonly IMapper _mapper;
+
+        public LoginService(ILoginRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public List<Person> GetLoginByCredentials(QueryParameters parameters)
+        {
+            return _repository.GetLoginByCredentials(parameters);
+        }
+    }
+}
