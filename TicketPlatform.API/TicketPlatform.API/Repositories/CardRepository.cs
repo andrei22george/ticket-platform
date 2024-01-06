bd07fe2dce0d229196f556b5cab5e3835fd9b11f
andrei22george <andrei22george@gmail.com>
2024-01-06 17:02:37 +0200

    Add repository for credit card

---
+++ TicketPlatform.API/TicketPlatform.API/Repositories/CardRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper.Contrib.Extensions;
+using ErrorOr;
+using Microsoft.Extensions.Options;
+using System.Data.SqlClient;
+using TicketPlatform.API.Model;
+using TicketPlatform.API.Repositories.Interfaces;
+using TicketPlatform.API.ServiceErrors;
+using TicketPlatform.API.Services;
+
+namespace TicketPlatform.API.Repositories
+{
+    public class CardRepository : ICardRepository
+    {
+        private readonly SqlConnection _sqlConnection;
+        private readonly Configurations _configuration;
+
+        public CardRepository(IOptions<Configurations> configurations)
+        {
+            _configuration = configurations.Value;
+            _sqlConnection = new SqlConnection(_configuration.ConnectionString);
+        }
+
+        public List<Card> GetAllCards(QueryParameters parameters)
+        {
+            return _sqlConnection.GetAll<Card>().ToList();
+        }
+
+        public ErrorOr<Card> GetCardById(int id)
+        {
+            var card = _sqlConnection.Get<Card>(id);
+
+            return card == null ? Errors.Card.NotFound : card;
+        }
+
+        public int InsertCard(Card card)
+        {
+            return (int)_sqlConnection.Insert(card);
+        }
+
+        public bool UpsertCard(int id, Card card)
+        {
+            card.Id = id;
+
+            return _sqlConnection.Update(card);
+        }
+
+        public bool DeleteCard(int id)
+        {
+            return _sqlConnection.Delete(new Card { Id = id });
+        }
+
+        public bool DeleteCards(List<int> ids)
+        {
+            var cardsToDelete = ids.Select(id => new Card { Id = id }).ToList();
+
+            return _sqlConnection.Delete(cardsToDelete);
+        }
+    }
+}
---
+++ TicketPlatform.API/TicketPlatform.API/Repositories/Interfaces/ICardRepository.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using TicketPlatform.API.Model;
+
+namespace TicketPlatform.API.Repositories.Interfaces
+{
+    public interface ICardRepository
+    {
+        public List<Card> GetAllCards(QueryParameters parameters);
+        public ErrorOr<Card> GetCardById(int id);
+        public int InsertCard(Card card);
+        public bool UpsertCard(int id, Card card);
+        public bool DeleteCard(int id);
+        public bool DeleteCards(List<int> ids);
+    }
+}
