a8571e509ca359f616b869e2fb92bbb416b7049d
andrei22george <andrei22george@gmail.com>
2024-01-06 17:56:36 +0200

    Add validators for cart, card and favourites

---
+++ TicketPlatform.API/TicketPlatform.API/Utilities/CardInValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TicketPlatform.API.Model.In;
+
+namespace TicketPlatform.API.Utilities
+{
+    public class CardInValidator : AbstractValidator<CardIn>
+    {
+        public CardInValidator()
+        {
+            RuleFor(cardIn => cardIn.Name).NotNull();
+            RuleFor(cardIn => cardIn.CardNumber).NotNull();
+            RuleFor(cardIn => cardIn.CVV).NotNull();
+            RuleFor(cardIn => cardIn.ExpDate).NotNull();
+        }
+    }
+}
