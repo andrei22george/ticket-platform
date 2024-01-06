abc1edf7438c61804103b7e2bd7cc29d40f86210
andrei22george <andrei22george@gmail.com>
2024-01-06 17:26:14 +0200

    Add repository for login

---
+++ TicketPlatform.API/TicketPlatform.API/Repositories/Interfaces/ILoginRepository.cs
@@ -0,0 +1,9 @@
+﻿using TicketPlatform.API.Model;
+
+namespace TicketPlatform.API.Repositories.Interfaces
+{
+    public interface ILoginRepository
+    {
+        public List<Person> GetLoginByCredentials(QueryParameters parameters);
+    }
+}
---
+++ TicketPlatform.API/TicketPlatform.API/Repositories/LoginRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Extensions.Options;
+using System.Data.SqlClient;
+using TicketPlatform.API.Model;
+using TicketPlatform.API.Repositories.Interfaces;
+using TicketPlatform.API.Services;
+
+namespace TicketPlatform.API.Repositories
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly SqlConnection _sqlConnection;
+        private readonly Configurations _configuration;
+
+        public LoginRepository(IOptions<Configurations> configurations)
+        {
+            _configuration = configurations.Value;
+            _sqlConnection = new SqlConnection(_configuration.ConnectionString);
+        }
+
+        public List<Person> GetLoginByCredentials(QueryParameters parameters)
+        {
+            var searchInAdminTable = _sqlConnection.Query<Person>("SELECT * FROM Admin WHERE email = @email AND password = @password",
+                new { email = parameters.Email, password = parameters.Password })
+                .ToList();
+
+            if (searchInAdminTable.Count > 0)
+            {
+                return searchInAdminTable;
+            }
+            else
+            {
+                return _sqlConnection.Query<Person>("SELECT * FROM [User] WHERE email = @email AND password = @password",
+                    new { email = parameters.Email, password = parameters.Password })
+                    .ToList();
+            }
+        }
+    }
+}
