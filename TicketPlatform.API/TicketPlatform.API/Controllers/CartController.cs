e7b74cc4e4ff949b532f23ea30e21dae3cd47911
andrei22george <andrei22george@gmail.com>
2024-01-06 17:57:19 +0200

    Add controllers for card, cart, favourites and login

---
+++ TicketPlatform.API/TicketPlatform.API/Controllers/CartController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using TicketPlatform.API.Model.In;
+using TicketPlatform.API.Model;
+using TicketPlatform.API.Services;
+using TicketPlatform.API.Utilities;
+using FluentValidation.Results;
+
+namespace TicketPlatform.API.Controllers
+{
+    [ApiController]
+    [Route("cart")]
+    public class CartController : ControllerBase
+    {
+        private readonly CartService cartService;
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(ILogger<CartController> logger, CartService service, IMapper mapper)
+        {
+            _logger = logger;
+            cartService = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cart> Get([FromQuery] QueryParameters parameters)
+        {
+            return cartService.GetAllCarts(parameters);
+        }
+
+        [HttpGet("{id}")]
+        public ErrorOr<Cart> GetCartById(int id)
+        {
+            return cartService.GetCartById(id);
+        }
+
+        [HttpPost]
+        public ErrorOr<int> CreateCart([FromBody] CartIn request)
+        {
+            var validator = new CartInValidator();
+            ValidationResult results = validator.Validate(request);
+
+            if (!results.IsValid)
+            {
+                foreach (var failure in results.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+            }
+
+            return cartService.InsertCart(request);
+        }
+
+        [HttpPut("{id}")]
+        public ErrorOr<bool> UpsertCart(int id, [FromBody] CartIn request)
+        {
+            var validator = new CartInValidator();
+            ValidationResult results = validator.Validate(request);
+
+            if (!results.IsValid)
+            {
+                foreach (var failure in results.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+            }
+
+            return cartService.UpsertCart(id, request);
+        }
+
+        [HttpDelete("{id}")]
+        public ErrorOr<bool> DeleteCart(int id)
+        {
+            return cartService.DeleteCart(id);
+        }
+
+        [HttpDelete]
+        public bool DeleteCarts([FromQuery] List<int> ids)
+        {
+            return cartService.DeleteCarts(ids);
+        }
+    }
+}
---
+++ TicketPlatform.API/TicketPlatform.API/Controllers/FavouritesController.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using TicketPlatform.API.Model.In;
+using TicketPlatform.API.Model;
+using TicketPlatform.API.Services;
+using TicketPlatform.API.Utilities;
+using FluentValidation.Results;
+using TicketPlatform.API.ServiceErrors;
+
+namespace TicketPlatform.API.Controllers
+{
+    [ApiController]
+    [Route("favourites")]
+    public class FavouritesController : ControllerBase
+    {
+        private readonly FavouritesService favouritesService;
+        private readonly ILogger<FavouritesController> _logger;
+
+        public FavouritesController(ILogger<FavouritesController> logger, FavouritesService service, IMapper mapper)
+        {
+            _logger = logger;
+            favouritesService = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Favourites> Get([FromQuery] QueryParameters parameters)
+        {
+            return favouritesService.GetAllFavouritess(parameters);
+        }
+
+        [HttpGet("{id}")]
+        public ErrorOr<Favourites> GetFavouritesById(int id)
+        {
+            return favouritesService.GetFavouritesById(id);
+        }
+
+        [HttpPost]
+        public ErrorOr<int> CreateFavourites([FromBody] FavouritesIn request)
+        {
+            var validator = new FavouritesInValidator();
+            ValidationResult results = validator.Validate(request);
+
+            if (!results.IsValid)
+            {
+                foreach (var failure in results.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+                return Errors.Favourites.FailedValidation;
+            }
+
+            if (StringValidationHelper.IsURL(request.Thumbnail))
+            {
+                return Errors.Favourites.InvalidURL;
+            }
+            else
+            {
+                return favouritesService.InsertFavourites(request);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ErrorOr<bool> UpsertFavourites(int id, [FromBody] FavouritesIn request)
+        {
+            var validator = new FavouritesInValidator();
+            ValidationResult results = validator.Validate(request);
+
+            if (!results.IsValid)
+            {
+                foreach (var failure in results.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+                return Errors.Favourites.FailedValidation;
+            }
+
+            if (StringValidationHelper.IsURL(request.Thumbnail))
+            {
+                return Errors.Favourites.InvalidURL;
+            }
+            else
+            {
+                return favouritesService.UpsertFavourites(id, request) == false ? Errors.Favourites.NotFound : true;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ErrorOr<bool> DeleteFavourites(int id)
+        {
+            return favouritesService.DeleteFavourites(id) == false ? Errors.Favourites.NotFound : true;
+        }
+
+        [HttpDelete]
+        public bool DeleteFavouritess([FromQuery] List<int> ids)
+        {
+            return favouritesService.DeleteFavouritess(ids);
+        }
+    }
+}
